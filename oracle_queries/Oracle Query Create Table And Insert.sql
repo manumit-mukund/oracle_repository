209726b107d98f937b0aa57c54d336ef115763ad
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2024-09-02 18:50:32 +0530

    Commit changes...

--- oracle_queries/Oracle Query Create Table And Insert.sql
+++ oracle_queries/Oracle Query Create Table And Insert.sql
@@ -1,20 +1,121 @@
-CREATE TABLE EMPLOYEE_INCOME
-(EMPID NUMBER(10),
-NAME VARCHAR2(20),
-SALARY NUMBER(10),
-department VARCHAR2(20));
+CREATE TABLE employee_income (
+    empid      NUMBER(10),
+    name       VARCHAR2(20),
+    salary     NUMBER(10),
+    department VARCHAR2(20)
+);
 
-INSERT INTO EMPLOYEE_INCOME (EMPID, NAME, SALARY, department) VALUES (1, 'Bill', 10000, 'HR');
-INSERT INTO EMPLOYEE_INCOME (EMPID, NAME, SALARY, department) VALUES (2, 'Mike', 20000, 'MARKETING');
-INSERT INTO EMPLOYEE_INCOME (EMPID, NAME, SALARY, department) VALUES (3, 'Samson', 30000, 'FINANCE');
-INSERT INTO EMPLOYEE_INCOME (EMPID, NAME, SALARY, department) VALUES (4, 'Kevin', 50000, 'IT');
+INSERT INTO employee_income (
+    empid,
+    name,
+    salary,
+    department
+) VALUES (
+    1,
+    'Bill',
+    10000,
+    'HR'
+);
 
-INSERT INTO EMPLOYEE_INCOME (EMPID, NAME, SALARY, department) VALUES (5, 'Bill1', 20000, 'HR');
-INSERT INTO EMPLOYEE_INCOME (EMPID, NAME, SALARY, department) VALUES (6, 'Mike1', 30000, 'MARKETING');
-INSERT INTO EMPLOYEE_INCOME (EMPID, NAME, SALARY, department) VALUES (7, 'Samson1', 40000, 'FINANCE');
-INSERT INTO EMPLOYEE_INCOME (EMPID, NAME, SALARY, department) VALUES (8, 'Kevin1', 60000, 'IT');
+INSERT INTO employee_income (
+    empid,
+    name,
+    salary,
+    department
+) VALUES (
+    2,
+    'Mike',
+    20000,
+    'MARKETING'
+);
 
-SELECT * FROM EMPLOYEE_INCOME;
+INSERT INTO employee_income (
+    empid,
+    name,
+    salary,
+    department
+) VALUES (
+    3,
+    'Samson',
+    30000,
+    'FINANCE'
+);
 
-SELECT * FROM EMPLOYEE_INCOME where (department, salary) in (select department, max (salary) from EMPLOYEE_INCOME group by department);
+INSERT INTO employee_income (
+    empid,
+    name,
+    salary,
+    department
+) VALUES (
+    4,
+    'Kevin',
+    50000,
+    'IT'
+);
 
+INSERT INTO employee_income (
+    empid,
+    name,
+    salary,
+    department
+) VALUES (
+    5,
+    'Bill1',
+    20000,
+    'HR'
+);
+
+INSERT INTO employee_income (
+    empid,
+    name,
+    salary,
+    department
+) VALUES (
+    6,
+    'Mike1',
+    30000,
+    'MARKETING'
+);
+
+INSERT INTO employee_income (
+    empid,
+    name,
+    salary,
+    department
+) VALUES (
+    7,
+    'Samson1',
+    40000,
+    'FINANCE'
+);
+
+INSERT INTO employee_income (
+    empid,
+    name,
+    salary,
+    department
+) VALUES (
+    8,
+    'Kevin1',
+    60000,
+    'IT'
+);
+
+SELECT
+    *
+FROM
+    employee_income;
+
+SELECT
+    *
+FROM
+    employee_income
+WHERE
+    ( department, salary ) IN (
+        SELECT
+            department, MAX(salary)
+        FROM
+            employee_income
+        GROUP BY
+            department
+    );
