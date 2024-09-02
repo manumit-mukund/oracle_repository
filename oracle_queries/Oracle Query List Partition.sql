9ceead38e3287a277e3eab2c18738e8de58960af
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2024-09-02 19:25:08 +0530

    Commit changes...

--- oracle_queries/Oracle Query List Partition.sql
+++ oracle_queries/Oracle Query List Partition.sql
@@ -1,41 +1,85 @@
 --------------------------------------------Start List partition-----------------------------------------
 
-CREATE TABLE sales_by_region ( 
-     product_id     NUMBER(6), 
-     quantity_sold  INTEGER, 
-     sale_date      DATE, 
-     store_name     VARCHAR(30), 
-     state_code     VARCHAR(2) 
-) 
- 
-     PARTITION BY LIST (state_code) 
-( 
-     PARTITION region_east 
-        VALUES ('CT','MA','MD','ME','NH','NJ','NY','PA','VA'), 
-     PARTITION region_west 
-        VALUES ('AZ','CA','CO','NM','NV','OR','UT','WA'), 
-     PARTITION region_south 
-        VALUES ('AL','AR','GA','KY','LA','MS','TN','TX'), 
-     PARTITION region_central 
-        VALUES ('IA','IL','MO','MI','ND','OH','SD') 
- )
- 
- SELECT TABLE_NAME,PARTITION_NAME, PARTITION_POSITION, HIGH_VALUE FROM USER_TAB_PARTITIONS WHERE TABLE_NAME ='SALES_BY_REGION'
- 
- ALTER TABLE sales_by_region 
-   ADD PARTITION region_nonmainland VALUES ('HI','PR')
-   
-   ALTER TABLE sales_by_region 
-   MODIFY PARTITION region_central 
-      ADD VALUES ('OK','KS')
-      
-      INSERT INTO sales_by_region VALUES (1001,100,'25-AUG-2014','My Store MA','MA')
-      
-      INSERT INTO sales_by_region VALUES (1002,200,'26-AUG-2014','My Store OK','OK')
-      
-      SELECT * FROM sales_by_region
-      
-      SELECT * FROM sales_by_region PARTITION(region_east)
+CREATE TABLE sales_by_region (
+    product_id    NUMBER(6),
+    quantity_sold INTEGER,
+    sale_date     DATE,
+    store_name    VARCHAR(30),
+    state_code    VARCHAR(2)
+)
+    PARTITION BY LIST ( state_code ) ( PARTITION region_east VALUES ( 'CT',
+    'MA',
+    'MD',
+    'ME',
+    'NH',
+    'NJ',
+    'NY',
+    'PA',
+    'VA' ),
+        PARTITION region_west VALUES ( 'AZ',
+        'CA',
+        'CO',
+        'NM',
+        'NV',
+        'OR',
+        'UT',
+        'WA' ),
+        PARTITION region_south VALUES ( 'AL',
+        'AR',
+        'GA',
+        'KY',
+        'LA',
+        'MS',
+        'TN',
+        'TX' ),
+        PARTITION region_central VALUES ( 'IA',
+        'IL',
+        'MO',
+        'MI',
+        'ND',
+        'OH',
+        'SD' )
+    );
+
+SELECT
+    table_name,
+    partition_name,
+    partition_position,
+    high_value
+FROM
+    user_tab_partitions
+WHERE
+    table_name = 'SALES_BY_REGION';
+
+ALTER TABLE sales_by_region ADD PARTITION region_nonmainland VALUES ( 'HI',
+'PR' );
+
+ALTER TABLE sales_by_region MODIFY PARTITION region_central ADD VALUES ( 'OK', 'KS' );
+
+INSERT INTO sales_by_region VALUES (
+    1001,
+    100,
+    '25-AUG-2014',
+    'My Store MA',
+    'MA'
+);
+
+INSERT INTO sales_by_region VALUES (
+    1002,
+    200,
+    '26-AUG-2014',
+    'My Store OK',
+    'OK'
+);
+
+SELECT
+    *
+FROM
+    sales_by_region;
+
+SELECT
+    *
+FROM
+    sales_by_region PARTITION ( region_east );
 
 --------------------------------------------End List partition-----------------------------------------
-
