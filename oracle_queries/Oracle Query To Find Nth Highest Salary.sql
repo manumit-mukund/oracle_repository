5bea19bd4f5a1992faf55cd5ff1b084c1d312187
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2024-09-03 22:38:48 +0530

    First commit...

---
+++ oracle_queries/Oracle Query To Find Nth Highest Salary.sql
@@ -0,0 +1,37 @@
+SELECT
+    name,
+    salary
+FROM
+    employee_income e1
+WHERE
+    2 - 1 = (
+        SELECT
+            COUNT(DISTINCT salary)
+        FROM
+            employee_income e2
+        WHERE
+            e2.salary > e1.salary
+    );
+    
+    
+----------------------for 2nd highest start-----------------------------
+
+SELECT
+    *
+FROM
+    employee_income
+WHERE
+    salary = (
+        SELECT
+            MAX(salary)
+        FROM
+            employee_income
+        WHERE
+            salary NOT IN (
+                SELECT
+                    MAX(salary)
+                FROM
+                    employee_income
+            )
+    );
+    ----------------------for 2nd highest end-----------------------------
