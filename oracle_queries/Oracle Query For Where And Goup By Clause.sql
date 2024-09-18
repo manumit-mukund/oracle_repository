3408fd4172006b092340d890bef729025818f801
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2024-09-18 14:06:54 +0530

    First commit...

---
+++ oracle_queries/Oracle Query For Where And Goup By Clause.sql
@@ -0,0 +1,73 @@
+DROP TABLE temp_employee;
+
+CREATE TABLE temp_employee (
+    id         NUMBER(5),
+    empname    VARCHAR2(100),
+    age        NUMBER(5),
+    empaddress VARCHAR2(100)
+);
+
+INSERT INTO temp_employee VALUES (
+    1,
+    'XYZ',
+    25,
+    'new road '
+);
+
+INSERT INTO temp_employee VALUES (
+    2,
+    'ABC',
+    28,
+    'old road'
+);
+
+INSERT INTO temp_employee VALUES (
+    3,
+    'AAB',
+    35,
+    'sector 23'
+);
+
+INSERT INTO temp_employee VALUES (
+    4,
+    'CBC',
+    25,
+    'sector 24'
+);
+
+SELECT
+    *
+FROM
+    temp_employee;
+
+SELECT
+    *
+FROM
+    temp_employee
+WHERE
+    id > 2;
+
+SELECT
+    *
+FROM
+    temp_employee
+WHERE
+    empname LIKE '%A%';
+
+SELECT
+    age,
+    COUNT(*) AS no_of_employees
+FROM
+    temp_employee
+GROUP BY
+    age;
+
+SELECT
+    age,
+    COUNT(*) AS no_of_employees
+FROM
+    temp_employee
+WHERE
+    empname LIKE '%B%'
+GROUP BY
+    age;
