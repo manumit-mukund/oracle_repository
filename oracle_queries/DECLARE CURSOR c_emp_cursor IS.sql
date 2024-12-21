1f4a7b11fe14731f8251df0a0a1028a7b04605d4
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2024-12-21 10:43:14 +0530

    Commit changes...

--- oracle_queries/DECLARE CURSOR c_emp_cursor IS.sql
+++ oracle_queries/DECLARE CURSOR c_emp_cursor IS.sql
@@ -1,4 +1,5 @@
 SET SERVEROUTPUT ON
+
 DECLARE
     CURSOR c_emp_cursor IS
     SELECT
@@ -10,17 +11,17 @@ DECLARE
         deptid = 30;
 
     v_empno employee_income.empid%TYPE;
-    v_lname employee_income.name%TYPE;
+    v_name  employee_income.name%TYPE;
 BEGIN
     OPEN c_emp_cursor;
     LOOP
         FETCH c_emp_cursor INTO
             v_empno,
-            v_lname;
+            v_name;
         EXIT WHEN c_emp_cursor%notfound;
         dbms_output.put_line(v_empno
                              || ' '
-                             || v_lname);
+                             || v_name);
     END LOOP;
 
     CLOSE c_emp_cursor;
