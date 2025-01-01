37bf19a87dc1be5362dcc2b9d1215408b65acb10
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-01-01 22:39:42 +0530

    Commit changes...

--- oracle_queries/CREATE BLOCKCHAIN TABLE bct_t1.sql
+++ oracle_queries/CREATE BLOCKCHAIN TABLE bct_t1.sql
@@ -17,6 +17,51 @@ no delete until 16 days after insert
     using
     "SHA2_512"
     version
-        "v1";
+"v1";
+
+set linesize 120 pagesize 50
+column column_name format a30
+column data_type format a27
+column hidden_column format a13
+
+SELECT
+    internal_column_id,
+    column_name,
+    data_type,
+    data_length,
+    hidden_column
+FROM
+    user_tab_cols
+WHERE
+    table_name = 'BCT_T1'
+ORDER BY
+    internal_column_id;
+
+column row_retention format a13
+column row_retention_locked format a20
+column table_inactivity_retention format a26
+column hash_algorithm format a14
+
+SELECT
+    row_retention,
+    row_retention_locked,
+    table_inactivity_retention,
+    hash_algorithm
+FROM
+    user_blockchain_tables
+WHERE
+    table_name = 'BCT_T1';
+
+INSERT INTO bct_t1 (
+    id,
+    fruit,
+    quantity,
+    created_date
+) VALUES (
+    1,
+    'apple',
+    20,
+    sysdate
+);
         
         
