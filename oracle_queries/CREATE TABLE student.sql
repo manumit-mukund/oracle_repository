11742ff41226cac3e11c3c24a6aef1baab68ffd5
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-02-26 23:03:00 +0530

    Commit new files...

---
+++ oracle_queries/CREATE TABLE student.sql
@@ -0,0 +1,61 @@
+CREATE TABLE student (
+    subject VARCHAR(10),
+    year    INT,
+    name    VARCHAR(10)
+);
+
+INSERT INTO student (
+    subject,
+    year,
+    name
+) VALUES ( 'English',
+           1,
+           'A' );
+
+INSERT INTO student (
+    subject,
+    year,
+    name
+) VALUES ( 'English',
+           2,
+           'B' );
+
+INSERT INTO student (
+    subject,
+    year,
+    name
+) VALUES ( 'English',
+           1,
+           'C' );
+
+INSERT INTO student (
+    subject,
+    year,
+    name
+) VALUES ( 'Math',
+           1,
+           'F' );
+
+INSERT INTO student (
+    subject,
+    year,
+    name
+) VALUES ( 'English',
+           1,
+           'D' );
+
+INSERT INTO student (
+    subject,
+    year,
+    name
+) VALUES ( 'English',
+           2,
+           'E' );
+
+INSERT INTO student (
+    subject,
+    year,
+    name
+) VALUES ( 'English',
+           2,
+           'E' );
---
+++ oracle_queries/DELETE duplicate FROM student.sql
@@ -0,0 +1,26 @@
+SELECT
+    subject,
+    year,
+    name
+FROM
+    student
+GROUP BY
+    subject,
+    year,
+    name
+HAVING
+    COUNT(*) > 1;
+
+DELETE FROM student
+WHERE
+    ( subject, year, name ) IN (
+        SELECT
+            subject, year, name
+        FROM
+            student
+        GROUP BY
+            subject,
+            year, name
+        HAVING
+            COUNT(*) > 1
+    );
