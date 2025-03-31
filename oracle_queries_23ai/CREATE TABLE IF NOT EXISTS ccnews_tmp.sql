7735b2f01aac6c52ade4fbcbb857048f96299878
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-03-31 15:07:51 +0530

    Commit...

--- oracle_queries_23ai/CREATE TABLE IF NOT EXISTS ccnews_tmp.sql
+++ oracle_queries_23ai/CREATE TABLE IF NOT EXISTS ccnews_tmp.sql
@@ -1,3 +1,5 @@
+DROP TABLE ccnews_tmp;
+
 CREATE TABLE IF NOT EXISTS ccnews_tmp (
     sentence VARCHAR2(4000)
 )
@@ -6,21 +8,22 @@ ORGANIZATION EXTERNAL (TYPE ORACLE_LOADER DEFAULT DIRECTORY dm_dump
                            (RECORDS DELIMITED BY 0x'0A'
                             READSIZE 100000000
                             FIELDS (sentence CHAR(4000 ) ) )
-    LOCATION ( dm_dump : 'dataset_200K.txt' )
+    LOCATION ( dm_dump : 'C:\app\admin\product\23ai\admin\free\dpdump/696168A276CE4123B9217F9EB76F43DF\dataset_200K.txt' )
 )
-    PARALLEL REJECT LIMIT UNLIMITED;
-    
+PARALLEL REJECT LIMIT
+unlimited;
 
-    
 SELECT
     COUNT(*)
 FROM
     ccnews_tmp;
-    
+
 SELECT
     *
 FROM
     ccnews_tmp
 WHERE
     ROWNUM < 4;
+
+COMMIT;
     
