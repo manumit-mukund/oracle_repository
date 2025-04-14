adf1d60a8a90cbd265674e8165ce0a39e5af1b9d
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-04-14 13:59:39 +0530

    Commit changes...

--- oracle_queries_23ai/CREATE TABLE mytab_v.sql
+++ oracle_queries_23ai/CREATE TABLE mytab_v.sql
@@ -6,15 +6,19 @@ CREATE TABLE mytab_v (
 
 describe mytab_v;
 
-variable myvar vector = '[1.1, 2.2, 3.0]';
+begin
 
+variable myvar vector = '[1.1, 2.2, 3.0]';
 INSERT INTO mytab_v VALUES ( :myvar );
+
+end;
+/
 
 SELECT
     COUNT(*)
 FROM
     mytab_v;
-    
+
 set linesize 150;
 
 SELECT
