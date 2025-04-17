72328dc9ab25584388aa2a67e7dd6b890ff29de8
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-04-17 17:39:54 +0530

    Commit a new file...

---
+++ oracle_queries_21c/CREATE PLUGGABLE DATABASE pdb2.sql
@@ -0,0 +1,28 @@
+CREATE PLUGGABLE DATABASE pdb2 ADMIN USER SYSDBA IDENTIFIED BY root 
+CREATE_FILE_DEST = 'C:\Users\admin\Downloads\WINDOWS.X64_213000_db_home\oradata';
+
+ALTER PLUGGABLE DATABASE pdb2 OPEN READ WRITE;
+
+COLUMN pdb_name FORMAT A20
+
+SELECT
+    pdb_name,
+    status
+FROM
+    dba_pdbs
+ORDER BY
+    pdb_name;
+    
+
+COLUMN name FORMAT A20
+
+SELECT
+    name,
+    open_mode
+FROM
+    v$pdbs
+ORDER BY
+    name;
+
+SHOW PDBS;
+
