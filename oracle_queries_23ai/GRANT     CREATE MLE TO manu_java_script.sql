d2e1bcf08f13322cf334b1aebce84b6c35bab511
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-04-18 11:33:59 +0530

    Commit a new file...

---
+++ oracle_queries_23ai/GRANT     CREATE MLE TO manu_java_script.sql
@@ -0,0 +1,21 @@
+GRANT
+    CREATE SESSION
+TO manu_java_script;
+
+GRANT
+    CREATE MLE
+TO manu_java_script;
+
+GRANT
+    CREATE PROCEDURE
+TO manu_java_script;
+
+GRANT EXECUTE ON javascript TO manu_java_script;
+
+GRANT
+    EXECUTE DYNAMIC MLE
+TO manu_java_script;
+
+GRANT db_developer_role TO manu_java_script;
+
+show parameter multilingual_engine;
