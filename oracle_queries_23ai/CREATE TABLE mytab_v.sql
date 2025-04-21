690ad3481d97296858c599df34954c5e1c55c4b0
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-04-21 13:26:57 +0530

    Commit changes...

--- oracle_queries_23ai/CREATE TABLE mytab_v.sql
+++ oracle_queries_23ai/CREATE TABLE mytab_v.sql
@@ -1,26 +1,24 @@
 DROP TABLE IF EXISTS mytab_v;
 
 CREATE TABLE mytab_v (
-    v64 VECTOR(3, FLOAT64)
+    v64 VECTOR(2, FLOAT64)
 );
 
 describe mytab_v;
 
 DECLARE
-    myvar vector;
-BEGIN
-    myvar := to_vector('[1.1, 2.2, 3.0]');
-    INSERT INTO mytab_v VALUES ( :myvar );
+    plsql_flt64 VECTOR := to_vector('[0.64, 0.96]');
+    
+begin
+INSERT INTO mytab_v VALUES ( :plsql_flt64 );
 
-END;
+end;
 /
 
 SELECT
     COUNT(*)
 FROM
     mytab_v;
-
-set linesize 150;
 
 SELECT
     *
