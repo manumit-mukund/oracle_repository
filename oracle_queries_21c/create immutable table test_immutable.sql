0ece0a7f6850f5e6bdae5da8335142e966b7ae18
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-04-22 13:00:00 +0530

    Commit changes..

--- oracle_queries_21c/create immutable table test_immutable.sql
+++ oracle_queries_21c/create immutable table test_immutable.sql
@@ -1,11 +1,22 @@
-ALTER SESSION SET container = pdb2;
+----------------------------------------Use con_SYS_PDB start---------------------------------------
+show con_name;
 
-create immutable table test_immutable (
+ALTER SESSION SET CONTAINER = pdb2;
+
+show con_name;
+
+ALTER PLUGGABLE DATABASE pdb2 OPEN READ WRITE;
+
+----------------------------------------Use con_SYS_PDB end-------------------------------------------
+DROP TABLE IF EXISTS test_immutable PURGE;
+
+CREATE immutable table test_immutable (
 eno number(10),
-name varchar2(20), 
-address varchar2(20), 
-created_date date, 
-constraint test_immutable_pk primary key (eno ) ) NO DROP UNTIL 10 DAYS IDLE NO DELETE UNTIL 30 DAYS AFTER INSERT;
+name varchar2(20),
+    address      VARCHAR2(20),
+    created_date DATE,
+    CONSTRAINT test_immutable_pk PRIMARY KEY ( eno )
+) NO DROP UNTIL 1 DAYS IDLE NO DELETE UNTIL 1 DAYS AFTER INSERT;
 desc dba_immutable_tables; 
 
 desc test_immutable;
@@ -15,33 +26,29 @@ SELECT
 FROM
     test_immutable;
 
-INSERT INTO test_immutable VALUES (
-    1,
-    'mir',
-    'india',
-    sysdate
-);
+INSERT INTO test_immutable VALUES ( 1,
+                                    'mir',
+                                    'india',
+                                    sysdate );
 
-INSERT INTO test_immutable VALUES (
-    2,
-    'Rakesh',
-    'England',
-    sysdate
-);
+INSERT INTO test_immutable VALUES ( 2,
+                                    'Rakesh',
+                                    'England',
+                                    sysdate );
 
 UPDATE test_immutable
 SET
-    eno = 3
+    eno = 2
 WHERE
-    name = 'Rakesh';
+    name = 'Rakesh1';
 
 DELETE FROM test_immutable
 WHERE
-    eno = 3;
+    eno = 2;
 
-ALTER TABLE test_immutable NO DELETE UNTIL 31 DAYS AFTER INSERT;
+ALTER TABLE test_immutable NO DELETE UNTIL 3 DAYS AFTER INSERT;
 
-ALTER TABLE test_immutable NO DELETE UNTIL 30 DAYS AFTER INSERT;
+ALTER TABLE test_immutable NO DELETE UNTIL 4 DAYS AFTER INSERT;
 
 ALTER TABLE test_immutable NO DROP;
 
