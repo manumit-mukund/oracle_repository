c077cd3f503216f3b83dcf02b44a06456fddac6c
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-05-06 13:12:57 +0530

    Commit changes...

--- oracle_queries_21c/ FOR i IN 1, REPEAT.sql
+++ oracle_queries_21c/ FOR i IN 1, REPEAT.sql
@@ -1,3 +1,7 @@
+-- Where it becomes interesting is when we add the REPEAT clause. 
+-- The upper limit on the iteration is defined by the WHILE clause. 
+-- In the following example each iteration increases the loop variable by multiplying the previous value by 5.
+
 BEGIN
     FOR i IN 1, REPEAT i * 5 WHILE i < 1000 LOOP
         dbms_output.put_line(i);
