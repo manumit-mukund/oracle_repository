5971b0741c371b89084e8ae9971aebe3dfedc0ea
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-05-06 12:54:20 +0530

    Commit changes...

--- oracle_queries_21c/FOR i IN 1..5 LOOP.sql
+++ oracle_queries_21c/FOR i IN 1..5 LOOP.sql
@@ -1,5 +1,6 @@
 set serveroutput on
 
+--Prior to Oracle database 21c
 BEGIN
     FOR i IN 1..5 LOOP
         dbms_output.put_line(i);
@@ -7,6 +8,8 @@ BEGIN
 END;
 /
 
+
+--From Oracle database 21c
 BEGIN
     FOR i IN REVERSE 1..5 LOOP
         dbms_output.put_line(i);
