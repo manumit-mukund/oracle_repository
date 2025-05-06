7ddc929108a385b7a0a86d1f32127f6d50b40f5e
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-05-06 13:10:26 +0530

    Commit changes...

--- oracle_queries_21c/FOR i IN 2 LOOP.sql
+++ oracle_queries_21c/FOR i IN 2 LOOP.sql
@@ -1,3 +1,6 @@
+-- Single Expression Iteration
+-- In its most basic form a single expression iteration is pretty useless, as it gives a single iteration.
+
 BEGIN
     FOR i IN 2 LOOP
         dbms_output.put_line(i);
