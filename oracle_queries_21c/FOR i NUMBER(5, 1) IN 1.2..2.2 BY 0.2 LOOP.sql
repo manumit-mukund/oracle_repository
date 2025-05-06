9b6b481ec283a76de258ab200292f2a3e9c6fc8b
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-05-06 13:08:23 +0530

    Commit changes...

--- oracle_queries_21c/FOR i NUMBER(5, 1) IN 1.2..2.2 BY 0.2 LOOP.sql
+++ oracle_queries_21c/FOR i NUMBER(5, 1) IN 1.2..2.2 BY 0.2 LOOP.sql
@@ -1,3 +1,5 @@
+--We can also increment or decrement by fractional values using the stepped range iteration syntax.
+
 BEGIN
     FOR i NUMBER(5, 1) IN 1.2..2.2 BY 0.2 LOOP
         dbms_output.put_line(i);
