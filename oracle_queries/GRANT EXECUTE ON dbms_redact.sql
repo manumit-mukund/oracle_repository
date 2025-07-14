94c3e7d033c19a92936874ba43f5f96483246297
manumit-mukund <manumit_mukund@yahoo.com>
2025-07-14 17:44:42 +0530

    Delete oracle_queries/GRANT EXECUTE ON dbms_redact.sql

--- oracle_queries/GRANT EXECUTE ON dbms_redact.sql
+++
@@ -1,9 +0,0 @@
-ALTER SESSION SET CONTAINER = pdb2;
-
-GRANT SELECT ON sys.redaction_policies TO manu_pdb;
-
-GRANT SELECT ON sys.redaction_columns TO manu_pdb;
-
-GRANT EXECUTE ON sys.dbms_redact TO manu_pdb;
-
-COMMIT;
