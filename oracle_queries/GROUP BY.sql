a26bcd054f0cd9274c2647bfdc3158c45d64ab4e
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-10-24 23:08:58 +0530

    Commit a new file...

---
+++ oracle_queries/GROUP BY.sql
@@ -0,0 +1,98 @@
+CREATE TABLE student (
+    name       VARCHAR(50),
+    course_fee DECIMAL(10, 2),
+    course     VARCHAR(50)
+);
+
+INSERT INTO student (
+    name,
+    course_fee,
+    course
+) VALUES ( 'Aleena',
+           5000.00,
+           'Mathematics' );
+
+INSERT INTO student (
+    name,
+    course_fee,
+    course
+) VALUES ( 'Ben',
+           6000.50,
+           'English' );
+
+INSERT INTO student (
+    name,
+    course_fee,
+    course
+) VALUES ( 'Chris',
+           7500.75,
+           'Science' );
+
+INSERT INTO student (
+    name,
+    course_fee,
+    course
+) VALUES ( 'Darla',
+           4500.25,
+           'Mathematics' );
+
+INSERT INTO student (
+    name,
+    course_fee,
+    course
+) VALUES ( 'Erie',
+           8000.00,
+           'English' );
+
+INSERT INTO student (
+    name,
+    course_fee,
+    course
+) VALUES ( 'Monty',
+           6500.00,
+           'Science' );
+
+INSERT INTO student (
+    name,
+    course_fee,
+    course
+) VALUES ( 'Aleena',
+           5500.50,
+           'Mathematics' );
+
+INSERT INTO student (
+    name,
+    course_fee,
+    course
+) VALUES ( 'Chris',
+           7200.75,
+           'Science' );
+
+INSERT INTO student (
+    name,
+    course_fee,
+    course
+) VALUES ( 'Darla',
+           4800.60,
+           'Mathematics' );
+
+INSERT INTO student (
+    name,
+    course_fee,
+    course
+) VALUES ( 'Ben',
+           8300.00,
+           'English' );
+
+SELECT
+    *
+FROM
+    student;
+
+SELECT
+    name,
+    SUM(course_fee)
+FROM
+    student
+GROUP BY
+    name;
