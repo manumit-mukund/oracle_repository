97f4661e816757023df1bc588475baf8a4c1d04e
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2026-02-09 22:45:19 +0530

    Commit changes...

--- oracle_queries/CREATE TABLESPACE.sql
+++ oracle_queries/CREATE TABLESPACE.sql
@@ -4,7 +4,7 @@ CREATE TABLESPACE hash_ptn_1
 
 CREATE TABLESPACE hash_ptn_2
     DATAFILE 'E:\Oracle Tablespace Files\hash_ptn_2.DBF' SIZE 500M REUSE
-    AUTOEXTEND ON NEXT 100M MAXSIZE 1000M;
+    AUTOEXTEND ON NEXT 100M MAXSIZE 1000M;E:\Oracle Tablespace Files
 
 CREATE TABLESPACE hash_ptn_3
     DATAFILE 'E:\Oracle Tablespace Files\hash_ptn_3.DBF' SIZE 500M REUSE
@@ -14,16 +14,6 @@ CREATE TABLESPACE hash_ptn_4
     DATAFILE 'E:\Oracle Tablespace Files\hash_ptn_4.DBF' SIZE 500M REUSE
     AUTOEXTEND ON NEXT 100M MAXSIZE 1000M;
 
-CREATE TABLE iot_incoming_data (
-    data_item_number    NUMBER,
-    data_item_key       VARCHAR2(32),
-    data_item_value     VARCHAR2(64),
-    data_item_timestamp TIMESTAMP
-)
-    PARTITION BY HASH ( data_item_number ) ( PARTITION p1 TABLESPACE hash_ptn_1,
-    PARTITION p2 TABLESPACE hash_ptn_2,
-    PARTITION p3 TABLESPACE hash_ptn_3,
-    PARTITION p4 TABLESPACE hash_ptn_4 );
 
 SELECT
     *
@@ -43,19 +33,6 @@ SELECT
 FROM
     dba_data_files;
 
-INSERT INTO iot_incoming_data
-    SELECT
-        ROWNUM,
-        dbms_random.string('X', 16),
-        dbms_random.string('X', 32),
-        systimestamp
-    FROM
-        dual
-    CONNECT BY
-        level < 10001;
-
-COMMIT;
-
 SELECT
     partition_name,
     num_rows
@@ -66,38 +43,3 @@ WHERE
 ORDER BY
     partition_position;
 
-SELECT
-    data_item_number
-FROM
-    iot_incoming_data PARTITION ( p1 )
-WHERE
-    ROWNUM < 6
-ORDER BY
-    1;
-
-SELECT
-    data_item_number
-FROM
-    iot_incoming_data PARTITION ( p2 )
-WHERE
-    ROWNUM < 6
-ORDER BY
-    1;
-
-SELECT
-    data_item_number
-FROM
-    iot_incoming_data PARTITION ( p3 )
-WHERE
-    ROWNUM < 6
-ORDER BY
-    1;
-
-SELECT
-    data_item_number
-FROM
-    iot_incoming_data PARTITION ( p4 )
-WHERE
-    ROWNUM < 6
-ORDER BY
-    1;
