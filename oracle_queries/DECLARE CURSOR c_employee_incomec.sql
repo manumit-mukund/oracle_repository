addbd17b01cb60989fa4e6b8f0037ae0781c6ae2
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2026-08-24 21:12:32 +0530

    Commit  a new file...

---
+++ oracle_queries/DECLARE CURSOR c_employee_incomec.sql
@@ -0,0 +1,38 @@
+set serveroutput on
+
+DECLARE
+    CURSOR c_employee_income IS
+    SELECT
+        empid,
+        name,
+        salary
+    FROM
+        employee_income
+    WHERE
+        deptid = 10;
+
+    v_emp_id employee_income.empid%TYPE;
+    v_name   employee_income.name%TYPE;
+    v_salary employee_income.salary%TYPE;
+    
+BEGIN
+    OPEN c_employee_income;
+    LOOP
+        FETCH c_employee_income INTO
+            v_emp_id,
+            v_name,
+            v_salary;
+        EXIT WHEN c_employee_income%notfound;
+        
+        dbms_output.put_line('ID: '
+                             || v_emp_id
+                             || ' | Name: '
+                             || v_name
+                             || ' | Salary: '
+                             || v_salary);
+
+    END LOOP;
+
+    CLOSE c_employee_income;
+END;
+/
