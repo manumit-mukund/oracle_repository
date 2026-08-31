21a76fd3325cc7f973ad9612a6d9f15889515918
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2026-08-31 12:46:04 +0530

    Commit filename change...

---
+++ oracle_queries/LIST PARTITION AUTOMATIC.sql
@@ -0,0 +1,75 @@
+--------------------------------------------Start AutoList partition-----------------------------------------
+DROP TABLE sales_auto_list PURGE;
+
+CREATE TABLE sales_auto_list (
+    salesman_id   NUMBER(5) NOT NULL,
+    salesman_name VARCHAR2(30),
+    sales_state   VARCHAR2(20) NOT NULL,
+    sales_amount  NUMBER(10),
+    sales_date    DATE NOT NULL
+)
+    PARTITION BY LIST ( sales_state ) AUTOMATIC ( PARTITION p_bh VALUES ( 'BIHAR' ) );
+
+SELECT
+    table_name,
+    partitioning_type,
+    autolist,
+    partition_count
+FROM
+    user_part_tables
+WHERE
+    table_name = 'SALES_AUTO_LIST';
+
+SELECT
+    table_name,
+    partition_name,
+    high_value
+FROM
+    user_tab_partitions
+WHERE
+    table_name = 'SALES_AUTO_LIST';
+
+INSERT INTO sales_auto_list VALUES ( 1,
+                                     'SM1',
+                                     'BIHAR',
+                                     1000,
+                                     TO_DATE('21-DEC-2025', 'DD-MON-YYYY') );
+
+INSERT INTO sales_auto_list VALUES ( 2,
+                                     'SM2',
+                                     'JHARKHAND',
+                                     2000,
+                                     TO_DATE('31-DEC-2025', 'DD-MON-YYYY') );
+
+INSERT INTO sales_auto_list VALUES ( 3,
+                                     'SM2',
+                                     'WEST BENGAL',
+                                     3000,
+                                     TO_DATE('11-JAN-2026', 'DD-MON-YYYY') );
+
+INSERT INTO sales_auto_list VALUES ( 4,
+                                     'SM3',
+                                     'BIHAR',
+                                     4000,
+                                     TO_DATE('18-JAN-2026', 'DD-MON-YYYY') );
+
+SELECT
+    table_name,
+    partition_name,
+    high_value
+FROM
+    user_tab_partitions
+WHERE
+    table_name = 'SALES_AUTO_LIST';
+
+SELECT
+    *
+FROM
+    sales_auto_list PARTITION ( p_bh );
+
+SELECT
+    *
+FROM
+    sales_auto_list PARTITION ( sys_p10093 ); -- 'sys_p10093' may vary based on the name prvided by the Oracle
+
+--------------------------------------------End AutoList partition-----------------------------------------
