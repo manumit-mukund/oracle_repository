7829881ca8447084c7c98bbd7005aff053d4a2a5
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2026-09-04 10:42:26 +0530

    Commit a new file...

---
+++ oracle_queries/Window Functions.sql
@@ -0,0 +1,6 @@
+SELECT 
+    name, deptid, salary,
+    ROW_NUMBER() OVER (PARTITION BY deptid ORDER BY salary DESC) as row_num,
+    RANK()       OVER (PARTITION BY deptid ORDER BY salary DESC) as rnk,
+    DENSE_RANK() OVER (PARTITION BY deptid ORDER BY salary DESC) as dense_rnk
+FROM employee_income;
