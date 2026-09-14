e4480c8e444b6b90acd9980b9a708c157127b9dc
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2026-09-14 22:44:37 +0530

    Commit changes...

--- oracle_queries/CONNECT BY.sql
+++ oracle_queries/CONNECT BY.sql
@@ -50,3 +50,17 @@ CONNECT BY
     PRIOR employee_id = manager_id
 ORDER SIBLINGS BY
     employee_name;
+
+SELECT
+    level AS sequence_number
+FROM
+    dual
+CONNECT BY
+    level <= 5;
+
+SELECT
+    trunc(sysdate, 'IW') + ( level - 1 ) AS week_date
+FROM
+    dual
+CONNECT BY
+    level <= 7;
