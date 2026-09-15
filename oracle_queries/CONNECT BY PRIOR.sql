ba41ccc97eec8a07db55201efd856792e58202e4
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2026-09-15 19:23:11 +0530

    Commit file rename...

---
+++ oracle_queries/CONNECT BY PRIOR.sql
@@ -0,0 +1,66 @@
+DROP TABLE employee_hierarchy PURGE; -- if exists
+
+CREATE TABLE employee_hierarchy (
+    employee_id   NUMBER PRIMARY KEY,
+    employee_name VARCHAR2(50),
+    manager_id    NUMBER
+);
+
+INSERT INTO employee_hierarchy VALUES ( 1,
+                                        'E1-CEO',
+                                        NULL );
+
+INSERT INTO employee_hierarchy VALUES ( 2,
+                                        'E2',
+                                        1 );
+
+INSERT INTO employee_hierarchy VALUES ( 3,
+                                        'E3',
+                                        1 );
+
+INSERT INTO employee_hierarchy VALUES ( 4,
+                                        'E4',
+                                        2 );
+
+INSERT INTO employee_hierarchy VALUES ( 5,
+                                        'E5',
+                                        4 );
+
+INSERT INTO employee_hierarchy VALUES ( 6,
+                                        'E6',
+                                        4 );
+
+COMMIT;
+
+SELECT
+    *
+FROM
+    employee_hierarchy;
+
+SELECT
+    level,
+    employee_id,
+    manager_id,
+    sys_connect_by_path(employee_name, ' -> ') AS reporting_tree
+FROM
+    employee_hierarchy
+START WITH
+    manager_id IS NULL
+CONNECT BY
+    PRIOR employee_id = manager_id
+ORDER SIBLINGS BY
+    employee_name;
+
+SELECT
+    level AS sequence_number
+FROM
+    dual
+CONNECT BY
+    level <= 5;
+
+SELECT
+    trunc(sysdate, 'IW') + ( level - 1 ) AS week_date
+FROM
+    dual
+CONNECT BY
+    level <= 7;
